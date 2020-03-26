0199782e7b2325cad658256b5716d7ed9c06dd78
李学江 <>
2020-03-26 09:45:02 +0800

    :fire: 添加commander js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-export * from './lib/async';
-export * from './lib/hash';
-export * from './lib/number';
+import { Command } from 'commander';
+
+const program = new Command();
+program
+  .version('0.1.0', '-v, --version')
+  .option('-i, --init', 'init something')
+  .option('-g, --generate', 'generate something')
+  .option('-r, --remove', 'remove something');
+
+// 必须在.parse()之前，因为node的emit()是即时的
+program.on('--help', function () {
+  console.log('  Examples:');
+  console.log('');
+  console.log('    this is an example');
+  console.log('');
+});
+
+program.parse(process.argv);
+
+if (program.init) {
+  console.log('init something')
+}
+
+if (program.generate) {
+  console.log('generate something')
+}
+
+if (program.remove) {
+  console.log('remove something')
+}
